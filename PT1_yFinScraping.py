ecf25438108635f2d1d638abdbc7553515af1f7b
MelHuney <mshi12@stevens.edu>
2025-11-18 19:45:43 -0500

    Wiki Done

--- PT1_yFinScraping.py
+++ PT1_yFinScraping.py
@@ -1,2 +1,14 @@
+# Imports
+
+import yfinance as yf
+
 def getFromYahooFinance(ticker):
     print(f"Getting data from Yahoo Finance for {ticker}...")
+
+for i, Row in DF.copy().iterrows():
+    YFTic = yf.ticker(Row.ticker)
+    if not 'longBusinessSummary' in YFTic.info:
+        YFTic = yf.ticker(Row.ticker.replace('.','-'))
+
+    try:
+        VCardColumns = ['address1', 'city', 'state', 'zip', 'country', 'phone', 'website', 'industry', 'industryKey', 'industryDisp']
